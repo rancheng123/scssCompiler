ad5d34099b7ec03a10baaab190b3d6aa0f374b39
rancheng <rancheng@jwzg.com>
2021-06-28 15:51:43 +0800

    modify

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+var styleTag = document.createElement('style');
+                 styleTag.innerText = ' div{width:100px;height:100px;}div aa{background:red;}div aa cc{background:red;}div bb{background:red;}div2{background:blue;width:100px;}div2 child{background:red;}div2 child2{background:red;}';
+                 document.body.appendChild(styleTag);console.log('a')
+console.log('b')
+
+
+
+
+
+alert(1)
+
+
